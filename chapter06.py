5130f4df3e75eb60d225f1e9b9b8f609dc8cc946
TKrizanovic <tomislavkrizanovic8@gmail.com>
2020-08-18 17:10:06 +0200

    chapter 6 finished

--- chapter06.py
+++ chapter06.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
 
+img = cv2.imread("Resources/lena.png")
+
+imgHor = np.hstack((img,img))
+imgVer = np.vstack((img,img))
 
 
-
+cv2.imshow("Horizontal stack", imgHor)
+cv2.imshow("Vertical stack", imgVer)
 
 
 
