dfc5c2dd16c6e3547035b739896629b996f3b226
TKrizanovic <tomislavkrizanovic8@gmail.com>
2020-08-18 11:21:50 +0200

    chapter 3 finished

--- chapter03.py
+++ chapter03.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
 
+img = cv2.imread("Resources/lena.png")
+print(img.shape)
+
+imgResize = cv2.resize(img, (240,240))
+
+cv2.imshow("Image", img)
+cv2.imshow("Image 2", imgResize)
+
+imgCropped = img[0:200,200:500]
+cv2.imshow("3", imgCropped)
+
+cv2.waitKey(0)
